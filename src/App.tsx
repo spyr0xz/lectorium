e7ef55388521203f28a3973ac6d3b398f1adf784
Nikita <123288888+spyr0xz@users.noreply.github.com>
2023-11-18 23:15:27 +0300

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="container mx-auto">
+     <p className='text-7xl'>Lets code</p>
     </div>
   );
 }
